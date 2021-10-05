d87e5027cff53788b53d9badecf8b355b6eb4c9a
jose teodoro da silva <jose.teodoro@dasadigital.com.br>
2021-10-05 18:33:47 -0300

    adding first running version for spring boot app

---
+++ src/main/java/br/jteodoro/wallet/WalletApplication.java
@@ -0,0 +1,13 @@
+package br.jteodoro.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApplication.class, args);
+	}
+
+}
