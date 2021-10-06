bbc6e66473d4abab6a02ffa03e2356065948fd3f
jose teodoro da silva <jose.teodoro@dasadigital.com.br>
2021-10-06 09:12:32 -0300

    adding swagger

--- src/main/java/br/jteodoro/wallet/WalletApplication.java
+++ src/main/java/br/jteodoro/wallet/WalletApplication.java
@@ -2,8 +2,13 @@ package br.jteodoro.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableCaching
 public class WalletApplication {
 
 	public static void main(String[] args) {
