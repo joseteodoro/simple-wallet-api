f07c6eacdce4ca5fac528044ff5617ae787b2609
jose teodoro da silva <jose.teodoro@dasadigital.com.br>
2021-10-06 14:58:16 -0300

    adding api test for health check

--- src/test/java/br/jteodoro/wallet/WalletApplicationTests.java
+++ src/test/java/br/jteodoro/wallet/WalletApplicationTests.java
@@ -3,11 +3,12 @@ package br.jteodoro.wallet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class WalletApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
---
+++ src/test/java/br/jteodoro/wallet/api/HealthCheckTests.java
@@ -0,0 +1,46 @@
+package br.jteodoro.wallet.api;
+
+import br.jteodoro.wallet.WalletApplication;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = WalletApplication.class)
+@AutoConfigureMockMvc
+@EnableAutoConfiguration
+public class HealthCheckTests {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    public void given_liveness_probe_when_get_then_status200()
+            throws Exception {
+        mvc.perform(get("/actuator/health")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}
